d0f08be62b9de0c0716c5a782ee93a58a28ec042
geothomas11 <geothms11@gmail.com>
2025-04-19 01:59:23 +0530

    updated files  go-grpc-product-svc

--- go-grpc-product-svc/pkg/services/product.go
+++ go-grpc-product-svc/pkg/services/product.go
@@ -1,1 +1,74 @@
 package services
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/geothomas11/go-grpc-product-svc/pkg/db"
+	"github.com/geothomas11/go-grpc-product-svc/pkg/models"
+	"github.com/geothomas11/go-grpc-product-svc/pkg/pb"
+)
+
+type Server struct {
+	H db.Handler
+	pb.UnimplementedProductServiceServer
+}
+
+func (s *Server) CreateProduct(ctx context.Context, req *pb.CreateProductRequest) (*pb.CreateProductResponse, error) {
+
+	var product models.Product
+
+	product.Name = req.Name
+	product.Stock = req.Stock
+	product.Price = req.Price
+
+	if result := s.H.DB.Create(&product); result.Error != nil {
+		return &pb.CreateProductResponse{
+			Status: http.StatusConflict,
+			Error:  result.Error.Error(),
+		}, nil
+	}
+	return &pb.CreateProductResponse{
+		Status: http.StatusCreated,
+		Id:     product.Id,
+	}, nil
+
+}
+
+func (s *Server) DecreaseStock(ctx context.Context, req *pb.DecreaseStockRequest) (*pb.DecreaseStockResponse, error) {
+	var product models.Product
+
+	if result := s.H.DB.First(&product, req.Id); result.Error != nil {
+		return &pb.DecreaseStockResponse{
+			Status: http.StatusNotFound,
+			Error:  result.Error.Error(),
+		}, nil
+	}
+	if product.Stock <= 0 {
+		return &pb.DecreaseStockResponse{
+			Status: http.StatusConflict,
+			Error:  "Stock is low",
+		}, nil
+	}
+
+	var log models.StockDecreaseLog
+	if result := s.H.DB.Where(&models.StockDecreaseLog{OrderId: req.OrderId}).First(&log); result.Error == nil {
+		return &pb.DecreaseStockResponse{
+			Status: http.StatusConflict,
+			Error:  "Stock already decreased",
+		}, nil
+	}
+
+	product.Stock = product.Stock - 1
+
+	s.H.DB.Save(&product)
+
+	log.OrderId = req.OrderId
+	log.ProductRefer = product.Id
+
+	s.H.DB.Create(&log)
+
+	return &pb.DecreaseStockResponse{
+		Status: http.StatusOK,
+	}, nil
+}
