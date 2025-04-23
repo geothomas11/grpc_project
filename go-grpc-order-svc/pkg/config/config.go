9420423eb11c6178fd269ee0361d4f180060c640
geothomas11 <geothms11@gmail.com>
2025-04-23 20:02:41 +0530

    updated files  go-grpc-order,api-gateway

--- go-grpc-order-svc/pkg/config/config.go
+++ go-grpc-order-svc/pkg/config/config.go
@@ -4,11 +4,11 @@ import "github.com/spf13/viper"
 
 type Config struct {
 	Port          string `mapstructure:"PORT"`
-	DBHost        string `mapstructure:"DBHost"`
-	DBPort        string `mapstructure:"DBPort"`
-	DBUser        string `mapstructure:"DBUser"`
-	DBPassword    string `mapstructure:"DBPassword"`
-	DBName        string `mapstructure:"DBName"`
+	DBHost        string `mapstructure:"DBHOST"`
+	DBPort        string `mapstructure:"DBPORT"`
+	DBUser        string `mapstructure:"DBUSER"`
+	DBPassword    string `mapstructure:"DBPASSWORD"`
+	DBName        string `mapstructure:"DBNAME"`
 	ProductSvcUrl string `mapstructure:"PRODUCT_SVC_URL"`
 }
 
@@ -16,7 +16,6 @@ func LoadConfig() (config Config, err error) {
 	viper.AddConfigPath("./pkg/config/envs")
 	viper.SetConfigName("dev")
 	viper.SetConfigType("env")
-
 	viper.AutomaticEnv()
 
 	err = viper.ReadInConfig()
